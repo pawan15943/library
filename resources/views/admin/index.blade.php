8d1aea300521ec190c3de69ffd62057f1a868299
Pawan Rathore <samvad.pawan@gmail.com>
2024-07-25 16:42:47 +0530

    Change Table Design 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,96 +1,218 @@
-
 @extends('layouts.admin')
 
 @section('title', 'Admin Dashboard')
 
 @section('content')
-    
-    <!-- Content Row -->
-    <div class="row">
 
-        <!-- Total Subscriptions -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold  text-uppercase mb-1">
-                                Full Day User</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$count_fullday}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
+<div class="row g-4">
+    <div class="col-lg-4">
+        <div class="dashibox">
+            <h4>Total Enrollments</h4>
+            <div class="d-flex">
+                <h2>1202</h2>
+                <a href=""><i class="fa fa-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="dashibox">
+            <h4>Total Course Completed</h4>
+            <div class="d-flex">
+                <h2>15</h2>
+                <a href=""><i class="fa fa-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="dashibox">
+            <h4>Total Certificate Distributed</h4>
+            <div class="d-flex">
+                <h2>10</h2>
+                <a href=""><i class="fa fa-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="dashibox">
+            <h4>Booked Seats </h4>
+            <div class="d-flex">
+                <h2>10</h2>
+                <a href=""><i class="fa fa-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="dashibox">
+            <h4>Available Seats</h4>
+            <div class="d-flex">
+                <h2>10</h2>
+                <a href=""><i class="fa fa-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="dashibox">
+            <h4>Monthly Revenue</h4>
+            <div class="d-flex">
+                <h2>10</h2>
+                <a href=""><i class="fa fa-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row mb-4">
+    <div class="col-lg-6">
+
+        <div class="course-list">
+            <h4>Course Wise Enrollments</h4>
+            <ul>
+                <li>
+                    <div class="d-flex">
+                        <h4>Course Name</h4>
+                        <div class="count">15</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="d-flex">
+                        <h4>Course Name</h4>
+                        <div class="count">10</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="d-flex">
+                        <h4>Course Name</h4>
+                        <div class="count">20</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="d-flex">
+                        <h4>Course Name</h4>
+                        <div class="count">18</div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-lg-6">
+
+        <div class="course-list">
+            <h4>Plan Wise Enrolments</h4>
+            <ul>
+                <li>
+                    <div class="d-flex">
+                        <h4>Full Day</h4>
+                        <div class="count">15</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="d-flex">
+                        <h4>First Half</h4>
+                        <div class="count">10</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="d-flex">
+                        <h4>Second Half</h4>
+                        <div class="count">20</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="d-flex">
+                        <h4>Hourly</h4>
+                        <div class="count">18</div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Content Row -->
+<div class="row">
+
+    <!-- Total Subscriptions -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold  text-uppercase mb-1">
+                            Full Day User</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$count_fullday}}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
                     </div>
                 </div>
             </div>
         </div>
-
-        <!-- Today's Subscriptions -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold  text-uppercase mb-1">
-                                First Half User</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$count_firstH}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Monthly Subscriptions -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold  text-uppercase mb-1">Second Half USer
-                            </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$count_secondH}}</div>
-                                </div>
-                                
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold  text-uppercase mb-1">Hourly USer
-                            </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$count_hourly}}</div>
-                                </div>
-                                
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        
     </div>
 
-  
-    
+    <!-- Today's Subscriptions -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold  text-uppercase mb-1">
+                            First Half User</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$count_firstH}}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Monthly Subscriptions -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold  text-uppercase mb-1">Second Half USer
+                        </div>
+                        <div class="row no-gutters align-items-center">
+                            <div class="col-auto">
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$count_secondH}}</div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold  text-uppercase mb-1">Hourly USer
+                        </div>
+                        <div class="row no-gutters align-items-center">
+                            <div class="col-auto">
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$count_hourly}}</div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+
+
 @endsection
